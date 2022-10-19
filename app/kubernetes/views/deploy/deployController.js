56e3aea934a40d48b383987cfde366b3e18478c6
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2022-10-19 18:34:55 +0300

    refactor(stacks): use box-selector for method

--- app/kubernetes/views/deploy/deployController.js
+++ app/kubernetes/views/deploy/deployController.js
@@ -8,7 +8,7 @@ import { KubernetesDeployManifestTypes, KubernetesDeployBuildMethods, Kubernetes
 import { renderTemplate } from '@/react/portainer/custom-templates/components/utils';
 import { isBE } from '@/portainer/feature-flags/feature-flags.service';
 import { compose, kubernetes } from '@@/BoxSelector/common-options/deployment-methods';
-import { editor, git, template, url } from '@@/BoxSelector/common-options/build-methods';
+import { editor, git, edgeStackTemplate, url } from '@@/BoxSelector/common-options/build-methods';
 
 class KubernetesDeployController {
   /* @ngInject */
@@ -36,7 +36,7 @@ class KubernetesDeployController {
       { ...git, value: KubernetesDeployBuildMethods.GIT },
       { ...editor, value: KubernetesDeployBuildMethods.WEB_EDITOR },
       { ...url, value: KubernetesDeployBuildMethods.URL },
-      { ...template, description: 'Use custom template', value: KubernetesDeployBuildMethods.CUSTOM_TEMPLATE },
+      { ...edgeStackTemplate, description: 'Use custom template', value: KubernetesDeployBuildMethods.CUSTOM_TEMPLATE },
     ];
 
     this.state = {
