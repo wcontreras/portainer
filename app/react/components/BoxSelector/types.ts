83c152a549d922e96469ae1acdd9330c05e6cb10
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2022-10-20 17:25:39 +0300

    refactor(init/admin): use box

--- app/react/components/BoxSelector/types.ts
+++ app/react/components/BoxSelector/types.ts
@@ -9,7 +9,7 @@ export type Value = number | string | boolean;
 export interface BoxSelectorOption<T extends Value> extends IconProps {
   readonly id: string;
   readonly label: string;
-  readonly description: ReactNode;
+  readonly description?: ReactNode;
   readonly value: T;
   readonly disabled?: () => boolean;
   readonly tooltip?: () => string;
